3142d1e64e35646fb5d9d565beeecc5821980fa3
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2021-05-18 12:28:50 +0100

    Remove blink sketch
    ESP32 DevKitC V4 does not have LED_BUILTIN

--- Control/src/main.cpp
+++ Control/src/main.cpp
@@ -3,13 +3,7 @@
 
 void setup() {
 	Serial.begin(115200);
-	pinMode(LED_BUILTIN, OUTPUT);
-	digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-	delay(2000);
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(2000);
-	digitalWrite(LED_BUILTIN, LOW);
 }
