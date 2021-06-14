aadb52990ee7d142e82c335f134f53e38d4e1133
Mee2001 <victor01.s@live.co.uk>
2021-06-14 17:25:57 +0100

    IMU stuff

---
+++ IMU/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <M5StickC.h>
+
+float pitch, roll, yaw;
+
+void setup() {
+  M5.begin();
+  M5.IMU.Init();
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, 26, 36);
+}
+void loop() {
+  M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf("%5.2f\n", roll);
+  Serial.printf("{\"cH\":%5.2f}\n", roll);
+  Serial1.printf("{\"cH\":%5.2f}\n", roll);
+  delay(50);
+}
